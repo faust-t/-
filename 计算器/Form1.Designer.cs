49d25b87218702cc2aff73dd0bef1a21b678dcf7
浮士德 <110364903+faust-t@users.noreply.github.com>
2022-08-01 10:50:11 +0800

    Add files via upload

---
+++ 计算器/Form1.Designer.cs
@@ -0,0 +1,315 @@
+﻿namespace 计算器
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnReduce = new System.Windows.Forms.Button();
+            this.btnRide = new System.Windows.Forms.Button();
+            this.btnExcept = new System.Windows.Forms.Button();
+            this.btnTakeMode = new System.Windows.Forms.Button();
+            this.btnNine = new System.Windows.Forms.Button();
+            this.btnEight = new System.Windows.Forms.Button();
+            this.btnSeven = new System.Windows.Forms.Button();
+            this.btnSix = new System.Windows.Forms.Button();
+            this.btnFive = new System.Windows.Forms.Button();
+            this.btnFour = new System.Windows.Forms.Button();
+            this.btnThree = new System.Windows.Forms.Button();
+            this.btnTwo = new System.Windows.Forms.Button();
+            this.btnOne = new System.Windows.Forms.Button();
+            this.spot = new System.Windows.Forms.Button();
+            this.btnZero = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnEqual = new System.Windows.Forms.Button();
+            this.labOutput = new System.Windows.Forms.Label();
+            this.labInput = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(95, 169);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(91, 62);
+            this.btnAdd.TabIndex = 2;
+            this.btnAdd.Text = "+";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnReduce
+            // 
+            this.btnReduce.Location = new System.Drawing.Point(204, 169);
+            this.btnReduce.Name = "btnReduce";
+            this.btnReduce.Size = new System.Drawing.Size(91, 62);
+            this.btnReduce.TabIndex = 3;
+            this.btnReduce.Text = "-";
+            this.btnReduce.UseVisualStyleBackColor = true;
+            this.btnReduce.Click += new System.EventHandler(this.btnReduce_Click);
+            // 
+            // btnRide
+            // 
+            this.btnRide.Location = new System.Drawing.Point(312, 169);
+            this.btnRide.Name = "btnRide";
+            this.btnRide.Size = new System.Drawing.Size(91, 62);
+            this.btnRide.TabIndex = 4;
+            this.btnRide.Text = "*";
+            this.btnRide.UseVisualStyleBackColor = true;
+            this.btnRide.Click += new System.EventHandler(this.btnRide_Click);
+            // 
+            // btnExcept
+            // 
+            this.btnExcept.Location = new System.Drawing.Point(424, 169);
+            this.btnExcept.Name = "btnExcept";
+            this.btnExcept.Size = new System.Drawing.Size(91, 62);
+            this.btnExcept.TabIndex = 5;
+            this.btnExcept.Text = "/";
+            this.btnExcept.UseVisualStyleBackColor = true;
+            this.btnExcept.Click += new System.EventHandler(this.btnExcept_Click);
+            // 
+            // btnTakeMode
+            // 
+            this.btnTakeMode.Location = new System.Drawing.Point(539, 169);
+            this.btnTakeMode.Name = "btnTakeMode";
+            this.btnTakeMode.Size = new System.Drawing.Size(91, 62);
+            this.btnTakeMode.TabIndex = 6;
+            this.btnTakeMode.Text = "%";
+            this.btnTakeMode.UseVisualStyleBackColor = true;
+            this.btnTakeMode.Click += new System.EventHandler(this.btnTakeMode_Click);
+            // 
+            // btnNine
+            // 
+            this.btnNine.Location = new System.Drawing.Point(312, 248);
+            this.btnNine.Name = "btnNine";
+            this.btnNine.Size = new System.Drawing.Size(91, 62);
+            this.btnNine.TabIndex = 9;
+            this.btnNine.Text = "9";
+            this.btnNine.UseVisualStyleBackColor = true;
+            this.btnNine.Click += new System.EventHandler(this.btnNine_Click);
+            // 
+            // btnEight
+            // 
+            this.btnEight.Location = new System.Drawing.Point(204, 248);
+            this.btnEight.Name = "btnEight";
+            this.btnEight.Size = new System.Drawing.Size(91, 62);
+            this.btnEight.TabIndex = 8;
+            this.btnEight.Text = "8";
+            this.btnEight.UseVisualStyleBackColor = true;
+            this.btnEight.Click += new System.EventHandler(this.btnEight_Click);
+            // 
+            // btnSeven
+            // 
+            this.btnSeven.Location = new System.Drawing.Point(95, 248);
+            this.btnSeven.Name = "btnSeven";
+            this.btnSeven.Size = new System.Drawing.Size(91, 62);
+            this.btnSeven.TabIndex = 7;
+            this.btnSeven.Text = "7";
+            this.btnSeven.UseVisualStyleBackColor = true;
+            this.btnSeven.Click += new System.EventHandler(this.btnSeven_Click);
+            // 
+            // btnSix
+            // 
+            this.btnSix.Location = new System.Drawing.Point(312, 330);
+            this.btnSix.Name = "btnSix";
+            this.btnSix.Size = new System.Drawing.Size(91, 62);
+            this.btnSix.TabIndex = 12;
+            this.btnSix.Text = "6";
+            this.btnSix.UseVisualStyleBackColor = true;
+            this.btnSix.Click += new System.EventHandler(this.btnSix_Click);
+            // 
+            // btnFive
+            // 
+            this.btnFive.Location = new System.Drawing.Point(204, 330);
+            this.btnFive.Name = "btnFive";
+            this.btnFive.Size = new System.Drawing.Size(91, 62);
+            this.btnFive.TabIndex = 11;
+            this.btnFive.Text = "5";
+            this.btnFive.UseVisualStyleBackColor = true;
+            this.btnFive.Click += new System.EventHandler(this.btnFive_Click);
+            // 
+            // btnFour
+            // 
+            this.btnFour.Location = new System.Drawing.Point(95, 330);
+            this.btnFour.Name = "btnFour";
+            this.btnFour.Size = new System.Drawing.Size(91, 62);
+            this.btnFour.TabIndex = 10;
+            this.btnFour.Text = "4";
+            this.btnFour.UseVisualStyleBackColor = true;
+            this.btnFour.Click += new System.EventHandler(this.btnFour_Click);
+            // 
+            // btnThree
+            // 
+            this.btnThree.Location = new System.Drawing.Point(312, 410);
+            this.btnThree.Name = "btnThree";
+            this.btnThree.Size = new System.Drawing.Size(91, 62);
+            this.btnThree.TabIndex = 15;
+            this.btnThree.Text = "3";
+            this.btnThree.UseVisualStyleBackColor = true;
+            this.btnThree.Click += new System.EventHandler(this.btnThree_Click);
+            // 
+            // btnTwo
+            // 
+            this.btnTwo.Location = new System.Drawing.Point(204, 410);
+            this.btnTwo.Name = "btnTwo";
+            this.btnTwo.Size = new System.Drawing.Size(91, 62);
+            this.btnTwo.TabIndex = 14;
+            this.btnTwo.Text = "2";
+            this.btnTwo.UseVisualStyleBackColor = true;
+            this.btnTwo.Click += new System.EventHandler(this.btnTwo_Click);
+            // 
+            // btnOne
+            // 
+            this.btnOne.Location = new System.Drawing.Point(95, 410);
+            this.btnOne.Name = "btnOne";
+            this.btnOne.Size = new System.Drawing.Size(91, 62);
+            this.btnOne.TabIndex = 13;
+            this.btnOne.Text = "1";
+            this.btnOne.UseVisualStyleBackColor = true;
+            this.btnOne.Click += new System.EventHandler(this.btnOne_Click);
+            // 
+            // spot
+            // 
+            this.spot.Location = new System.Drawing.Point(312, 493);
+            this.spot.Name = "spot";
+            this.spot.Size = new System.Drawing.Size(91, 62);
+            this.spot.TabIndex = 18;
+            this.spot.Text = ".";
+            this.spot.UseVisualStyleBackColor = true;
+            this.spot.Click += new System.EventHandler(this.spot_Click);
+            // 
+            // btnZero
+            // 
+            this.btnZero.Location = new System.Drawing.Point(95, 493);
+            this.btnZero.Name = "btnZero";
+            this.btnZero.Size = new System.Drawing.Size(200, 62);
+            this.btnZero.TabIndex = 17;
+            this.btnZero.Text = "0";
+            this.btnZero.UseVisualStyleBackColor = true;
+            this.btnZero.Click += new System.EventHandler(this.btnZero_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(424, 248);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(91, 144);
+            this.btnDelete.TabIndex = 16;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // btnEqual
+            // 
+            this.btnEqual.Location = new System.Drawing.Point(539, 248);
+            this.btnEqual.Name = "btnEqual";
+            this.btnEqual.Size = new System.Drawing.Size(91, 307);
+            this.btnEqual.TabIndex = 19;
+            this.btnEqual.Text = "=";
+            this.btnEqual.UseVisualStyleBackColor = true;
+            this.btnEqual.Click += new System.EventHandler(this.btnEqual_Click);
+            // 
+            // labOutput
+            // 
+            this.labOutput.Location = new System.Drawing.Point(95, 24);
+            this.labOutput.Name = "labOutput";
+            this.labOutput.Size = new System.Drawing.Size(535, 44);
+            this.labOutput.TabIndex = 20;
+            // 
+            // labInput
+            // 
+            this.labInput.Location = new System.Drawing.Point(95, 85);
+            this.labInput.Name = "labInput";
+            this.labInput.Size = new System.Drawing.Size(535, 46);
+            this.labInput.TabIndex = 21;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(424, 410);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(91, 144);
+            this.button1.TabIndex = 22;
+            this.button1.Text = "empty";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(775, 611);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.labInput);
+            this.Controls.Add(this.labOutput);
+            this.Controls.Add(this.btnEqual);
+            this.Controls.Add(this.spot);
+            this.Controls.Add(this.btnZero);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnThree);
+            this.Controls.Add(this.btnTwo);
+            this.Controls.Add(this.btnOne);
+            this.Controls.Add(this.btnSix);
+            this.Controls.Add(this.btnFive);
+            this.Controls.Add(this.btnFour);
+            this.Controls.Add(this.btnNine);
+            this.Controls.Add(this.btnEight);
+            this.Controls.Add(this.btnSeven);
+            this.Controls.Add(this.btnTakeMode);
+            this.Controls.Add(this.btnExcept);
+            this.Controls.Add(this.btnRide);
+            this.Controls.Add(this.btnReduce);
+            this.Controls.Add(this.btnAdd);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnReduce;
+        private System.Windows.Forms.Button btnRide;
+        private System.Windows.Forms.Button btnExcept;
+        private System.Windows.Forms.Button btnTakeMode;
+        private System.Windows.Forms.Button btnNine;
+        private System.Windows.Forms.Button btnEight;
+        private System.Windows.Forms.Button btnSeven;
+        private System.Windows.Forms.Button btnSix;
+        private System.Windows.Forms.Button btnFive;
+        private System.Windows.Forms.Button btnFour;
+        private System.Windows.Forms.Button btnThree;
+        private System.Windows.Forms.Button btnTwo;
+        private System.Windows.Forms.Button btnOne;
+        private System.Windows.Forms.Button spot;
+        private System.Windows.Forms.Button btnZero;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnEqual;
+        private System.Windows.Forms.Label labOutput;
+        private System.Windows.Forms.Label labInput;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ 计算器/Form1.cs
@@ -0,0 +1,380 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace 计算器
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //定义一个字符串
+        string input = null;
+        //结果
+        double num = 0;
+        
+        /// <summary>
+        /// 加
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            labInput.Text = labInput.Text + "+";
+            input = input + "+" + " ";
+        }
+
+        /// <summary>
+        /// 减
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnReduce_Click(object sender, EventArgs e)
+        {
+            labInput.Text = labInput.Text + "-";
+            input = input + "-" + " ";
+        }
+
+        /// <summary>
+        /// 乘
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnRide_Click(object sender, EventArgs e)
+        {
+            labInput.Text = labInput.Text + "*";
+            input = input + "*" + " ";
+        }
+
+        /// <summary>
+        /// 除
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnExcept_Click(object sender, EventArgs e)
+        {
+            labInput.Text = labInput.Text + "/";
+            input = input + "/" + " ";
+        }
+
+        /// <summary>
+        /// 取模
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnTakeMode_Click(object sender, EventArgs e)
+        {
+            labInput.Text = labInput.Text + "%";
+            input = input + "%" + " ";
+        }
+
+        /// <summary>
+        /// 小数点
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void spot_Click(object sender, EventArgs e)
+        {
+            labInput.Text = labInput.Text + ".";
+            input = input + "." + " ";
+        }
+
+        /// <summary>
+        /// 数字0
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnZero_Click(object sender, EventArgs e)
+        {
+            labInput.Text = labInput.Text + "0";
+            input = input + "0" + " ";
+        }
+        /// <summary>
+        /// 数字1
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnOne_Click(object sender, EventArgs e)
+        {
+            labInput.Text = labInput.Text + "1";
+            input = input + "1" + " ";
+        }
+        /// <summary>
+        /// 数字2
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnTwo_Click(object sender, EventArgs e)
+        {
+            labInput.Text = labInput.Text + "2";
+            input = input + "2" + " ";
+        }
+        /// <summary>
+        /// 数字3
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnThree_Click(object sender, EventArgs e)
+        {
+            labInput.Text = labInput.Text + "3";
+            input = input + "3" + " ";
+        }
+        /// <summary>
+        /// 数字4
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnFour_Click(object sender, EventArgs e)
+        {
+            labInput.Text = labInput.Text + "4";
+            input = input + "4" + " ";
+        }
+        /// <summary>
+        /// 数字5
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnFive_Click(object sender, EventArgs e)
+        {
+            labInput.Text = labInput.Text + "5";
+            input = input + "5" + " ";
+        }
+        /// <summary>
+        /// 数字6
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnSix_Click(object sender, EventArgs e)
+        {
+            labInput.Text = labInput.Text + "6";
+            input = input + "6" + " ";
+        }
+        /// <summary>
+        /// 数字7
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnSeven_Click(object sender, EventArgs e)
+        {
+            labInput.Text = labInput.Text + "7";
+            input = input + "7" + " ";
+        }
+        /// <summary>
+        /// 数字8
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnEight_Click(object sender, EventArgs e)
+        {
+            labInput.Text = labInput.Text + "8";
+            input = input + "8" + " ";
+        }
+        /// <summary>
+        /// 数字9
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnNine_Click(object sender, EventArgs e)
+        {
+            labInput.Text = labInput.Text + "9";
+            input = input + "9" + " ";
+        }
+
+        /// <summary>
+        /// 加载
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.btnEqual.Select();
+        }
+
+        /// <summary>
+        /// 等于 
+        /// 输出结果
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnEqual_Click(object sender, EventArgs e)
+        {
+            string str = null;
+
+            //首先判断输入的字符长度
+            if (input.Length > 0)
+            {
+                //判断输入的最后一个字符是不是运算符 若果是就直接删除掉
+                input = JudgeSpace(input);
+            }
+
+            //以空格做分隔符 第一次处理输入的字符
+            string[] firstString = input.Split(' ');
+            //1.拼接输入的算式
+            str = InputLine(firstString);
+            //2.优先执行 乘 除 取模 运算
+            string[] secondString = str.Split(' ');
+            str = FirstOperation(secondString);
+            //str = str.Substring(2, str.Length);
+            //3.执行加减运算
+            string[] finalString = str.Split(' ');
+            num = SecondOperation(finalString);
+            //输出结果
+            labOutput.Text = Convert.ToString(num);
+
+        }
+
+        /// <summary>
+        /// 判断输入的最后一个字符是不是运算符 若果是就直接删除掉
+        /// </summary>
+        /// <param name="input"></param>
+        /// <returns></returns>
+        public string JudgeSpace(string input)
+        {
+            if (input[input.Length - 2] == '+' | input[input.Length - 2] == '-' | input[input.Length - 2] == '*' | input[input.Length - 2] == '/' | input[input.Length - 2] == '%')
+            {
+                input = input.Substring(0, input.Length - 2);
+            }
+            return input;
+        }
+
+        /// <summary>
+        /// 删除
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            if (labInput.Text.Length > 0)
+            {
+                labInput.Text = labInput.Text.Substring(0, labInput.Text.Length - 1);
+                input = input.Substring(0, input.Length - 2);
+            }
+        }
+
+        /// <summary>
+        /// 清空
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button1_Click(object sender, EventArgs e)
+        { 
+            labInput.Text = "";
+            labOutput.Text = "";
+            input = "";
+        }
+
+        /// <summary>
+        /// 3.执行加减
+        /// </summary>
+        /// <param name="finalString"></param>
+        /// <returns></returns>
+        public double SecondOperation(string[] finalString)
+        {
+
+            if (finalString.Length > 1)
+            {
+                for (int i = 1; i < finalString.Length; i++)
+                {
+                    if (finalString[i] == "+")
+                    {
+                        num = Convert.ToDouble(finalString[i - 1]) + Convert.ToDouble(finalString[i + 1]);
+                        finalString[i + 1] = num.ToString();
+                    }
+                    else if (finalString[i] == "-")
+                    {
+                        num = Convert.ToDouble(finalString[i - 1]) - Convert.ToDouble(finalString[i + 1]);
+                        finalString[i + 1] = num.ToString();
+                    }
+
+                }
+            }
+            else
+            {
+                num = Convert.ToDouble(finalString[0]);
+            }
+            return num;
+        }
+
+        /// <summary>
+        /// 2.优先执行乘除取模
+        /// </summary>
+        /// <param name="secondString"></param>
+        /// <returns></returns>
+        public string FirstOperation(string[] secondString)
+        {
+            //声明一个动态数组
+            ArrayList list = new ArrayList();
+            //*/%算式
+            string s = "";
+            for (int i = 0; i < secondString.Length; i++)
+            {
+                if (secondString[i] == "*")
+                {
+                    num = Convert.ToDouble(secondString[i - 1]) * Convert.ToDouble(secondString[i + 1]);
+                    secondString[i + 1] = num.ToString();
+                    if (list.Count > 0)
+                    {
+                        list.RemoveAt(list.Count - 1);
+                    }
+                }
+                else if (secondString[i] == "/")
+                {
+                    num = Convert.ToDouble(secondString[i - 1]) / Convert.ToDouble(secondString[i + 1]);
+                    secondString[i + 1] = num.ToString();
+                    if (list.Count > 0)
+                    {
+                        list.RemoveAt(list.Count - 1);
+                    }
+                }
+                else if (secondString[i] == "%")
+                {
+                    num = Convert.ToDouble(secondString[i - 1]) % Convert.ToDouble(secondString[i + 1]);
+                    secondString[i + 1] = num.ToString();
+                    if (list.Count > 0)
+                    {
+                        list.RemoveAt(list.Count - 1);
+                    }
+                }
+                else
+                {
+                    list.Add(secondString[i]);
+                }
+            }
+            s = string.Join(" ", (string[])list.ToArray(typeof(string)));
+            return s;
+        }
+
+        /// <summary>
+        /// 1.拼接输入的算式
+        /// </summary>
+        /// <param name="firstString"></param>
+        /// <returns></returns>
+        public string InputLine(string[] firstString)
+        {
+            string str = null;
+            for (int i = 0; i < firstString.Length; i++)
+            {
+                //当这个字符不是 +-*/% 时直接拼接
+                if (firstString[i] != "+" && firstString[i] != "-" && firstString[i] != "*" && firstString[i] != "/" && firstString[i] != "%")
+                {
+                    str = str + firstString[i];
+                }
+                else
+                {
+                    str = str + " " + firstString[i] + " ";
+                }
+            }
+            return str;
+        }
+    }
+}
